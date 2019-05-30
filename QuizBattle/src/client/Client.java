3cfb7d46154bb94c867c906456c1bcc0eae71e96
krieg <kriveb15@htlkaindorf.at>
2019-05-30 18:49:32 +0200

    client

--- QuizBattle/src/client/Client.java
+++ QuizBattle/src/client/Client.java
@@ -6,13 +6,11 @@ import beans.Question;
 import configFiles.Config;
 import gui.ChooseCategory;
 import start.GUIBuilder;
-import gui.QuestionView;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,19 +23,23 @@ public class Client {
     private ObjectInputStream ois;
     private ObjectOutputStream oos;
     private int highestUserID;
-    private String errorType;
     private GUIBuilder gui;
     private ChooseCategory choosecategory;
+    private int count = 0;
+    private String myUsername, usernameFromOpponent;
+    private int scorePlayerOne = 0;
+    private int scorePlayerTwo = 0;
+    private Socket socket;
 
     public Client() {
         connect();
     }
-    
-    public int getScorePlayerOne(){
+
+    public int getScorePlayerOne() {
         return scorePlayerOne;
     }
-    
-    public int getScorePlayerTwo(){
+
+    public int getScorePlayerTwo() {
         return scorePlayerTwo;
     }
 
@@ -45,15 +47,16 @@ public class Client {
         return highestUserID;
     }
 
-    public String getErrorType() {
-        return errorType;
-    }
-
     private void log(String message) {
         System.out.println(message);
     }
 
+    public void setGUIBuilder(GUIBuilder gui) {
+        this.gui = gui;
+    }
+
     public void setAnswer(String answer) throws IOException {
+        //Schickt die angeklickte Antwort zum Server
         gui.closeQuestionView();
         oos.writeObject(answer);
         oos.flush();
@@ -66,16 +69,14 @@ public class Client {
         } catch (IOException ex) {
             log("Exception: unable to logout");
         }
-
     }
 
     public void startGame() {
+        //Schickt dem Server eine Nachricht, dass der Client ein Spiel starten möchte
         try {
             gui.openLoadingViewGUI();
             oos.writeObject("startgame");
             oos.flush();
-            GameCommunication gameCommunication = new GameCommunication();
-            gameCommunication.start();
         } catch (IOException ex) {
             log("Exception: unable to start game");
         }
@@ -90,10 +91,6 @@ public class Client {
         } catch (IOException ex) {
             log("Exception: unable to sign up");
         }
-    }
-
-    public void setGUIBuilder(GUIBuilder gui) {
-        this.gui = gui;
     }
 
     public void choosedCategoryName(String categoryName) {
@@ -116,17 +113,15 @@ public class Client {
             log("Exception: unable to login");
         }
     }
-    private Socket socket;
 
     public void connect() {
-        /* Verbindung zum Server herstellen */
+        // Verbindung zum Server herstellen
         try {
-            socket = new Socket(Config.IP_ADDRESS, 9999);
+            socket = new Socket(Config.IP_ADDRESS, 9999); //Holt sich die IP-Adresse aus dem Config-File
             oos = new ObjectOutputStream(socket.getOutputStream());
             ois = new ObjectInputStream(socket.getInputStream());
             ServerMessages sm = new ServerMessages();
             sm.start();
-
         } catch (IOException ex) {
             log("Exception: unable to connect to server");
         }
@@ -141,79 +136,52 @@ public class Client {
         }
     }
 
-    class GameCommunication extends Thread {
-
-        @Override
-        public void run() {
-            try {
-                while (true) {
-                    Scanner sc = new Scanner(System.in);
-                    String answerQuestion = sc.nextLine();
-                    System.out.println(answerQuestion);
-                    oos.writeObject(answerQuestion);
-                    oos.flush();
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-
-    }
-    private QuestionView questionview;
-    private int count = 0;
-    private String username, usernameFromOpponent;
-    private int scorePlayerOne = 0;
-    private int scorePlayerTwo = 0;
     class ServerMessages extends Thread {
 
-        /* Thread zur Kommunikation mit dem Server */
+        // Thread zur Kommunikation mit dem Server
         @Override
         public void run() {
             try {
                 while (true) {
                     String message = (String) ois.readObject();
                     if (message.equals("failed")) {
-                        System.out.println("hier");
                         gui.openStartPage(true);
                         log("Login or registration failed!");
                     } else if (message.equals("loggedin")) {
-                        username = (String) ois.readObject();
+                        myUsername = (String) ois.readObject();
                         gui.openStartGame();
                         log("You are logged in!");
                     } else if (message.equals("wait")) {
+                        //Setzte den Player in den Wartezustand
                         scorePlayerOne = (int) ois.readObject();
                         scorePlayerTwo = (int) ois.readObject();
                         gui.closeLoadingView();
-                        gui.openBattleView(username, usernameFromOpponent, scorePlayerOne, scorePlayerTwo);
+                        gui.openBattleView(myUsername, usernameFromOpponent, scorePlayerOne, scorePlayerTwo);
                     } else if (message.equals("choose category")) {
+                        //Aufruf der GUI um eine Kategorie auszuwählen
                         gui.closeBattleView();
                         ArrayList<Category> categories = (ArrayList) ois.readObject();
                         gui.closeLoadingView();
                         choosecategory = gui.openChooseCategoryGUI(categories);
-                    }else if(message.equals("battleview")){
-                        scorePlayerOne = (int)ois.readObject();
-                        scorePlayerTwo = (int)ois.readObject();
-                        if(gui.isBattleViewOpen())
-                        {
+                    } else if (message.equals("battleview")) {
+                        //Weckt den Player vom Wartezustand auf und er ist andere zu spielen
+                        scorePlayerOne = (int) ois.readObject();
+                        scorePlayerTwo = (int) ois.readObject();
+                        if (gui.isBattleViewOpen()) {
                             gui.closeBattleView();
                         }
-                        
                         gui.closeLoadingView();
-                        gui.openBattleView(username, usernameFromOpponent, scorePlayerOne, scorePlayerTwo);
+                        gui.openBattleView(myUsername, usernameFromOpponent, scorePlayerOne, scorePlayerTwo);
                         gui.setButton();
-                    }
-                    else if (message.equals("question")) {
-                        
+                    } else if (message.equals("question")) {
+                        //Zeigt dem User die Question an
                         Question question = (Question) ois.readObject();
                         if (count == 0) {
                             count++;
                         }
                         gui.closeBattleView();
-
                         gui.openQuestionView(question);
-                        if (choosecategory == null) {
-
-                        } else {
+                        if(choosecategory != null) {
                             gui.closeCategoryView(choosecategory);
                         }
                     } else if (message.equals("usernameOpponent")) {
@@ -225,21 +193,16 @@ public class Client {
                         gui.closeBattleView();
                         gui.openLoserView();
                     } else if (message.equals("equal")) {
+                        //Unentschieden
                         gui.closeBattleView();
                         gui.openEqualView();
                     } else {
                         log(message);
-                    }
+                    } 
                 }
             } catch (IOException | ClassNotFoundException ex) {
                 log("Exception: unable to read received object");
             }
         }
     }
-    /*
-     public static void main(String[] args) {
-     Client c = new Client();
-     c.login(new Account("adsf", "sadf", null, 0, LocalDate.MIN));
-     c.startGame();
-     } */
 }
