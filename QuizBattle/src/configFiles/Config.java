999a7e2702c48de24055efc166298241fe04b2c6
krieg <kriveb15@htlkaindorf.at>
2019-06-03 10:33:03 +0200

    config File

--- QuizBattle/src/configFiles/Config.java
+++ QuizBattle/src/configFiles/Config.java
@@ -5,7 +5,7 @@ public interface Config {
     String DB_DATABASE_NAME = "QuizBattle";
     String DB_URL = "jdbc:postgresql://localhost/";
     String DB_USERNAME = "postgres";
-    String DB_PASSWD = "VK090401";
+    String DB_PASSWD = "postgres";
     String DB_DRIVER = "org.postgresql.Driver";
     //Server-Client
     String IP_ADDRESS = "172.20.10.10";
